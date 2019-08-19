80d73caf2095b903c7b00f4e2f49489b18add035
Jose Miguel <53483639+Fortroya@users.noreply.github.com>
2019-08-18 20:02:26 -0600

    Se hicieron unos cambios mas

--- LibroCalificaciones.cpp
+++ LibroCalificaciones.cpp
@@ -6,61 +6,61 @@ LibroCalificaciones::LibroCalificaciones() {
 }
 
 LibroCalificaciones::LibroCalificaciones(const std::string& nombreCurso, int calificaciones[][EXAMENES]) {
-    setNombreCurso(nombreCurso);
+	setNombreCurso(nombreCurso);
 
-    for (int est = 0; est < ESTUDIANTES; est++) {
-        for (int ex = 0; ex < EXAMENES; ex++) {
+	for (int est = 0; est < ESTUDIANTES; est++) {
+		for (int ex = 0; ex < EXAMENES; ex++) {
 
-            calificaciones[estudiantes][examenes] = calificaciones[estudiantes][examenes];
-        }
-    }
+			calificaciones[est][ex] = calificaciones[est][ex];
+		}
+	}
 }
 
 int LibroCalificaciones::obtenerNotaMinima() {
-    int notaMinima = 0;
-    for (int estudiantes = 0; estudiantes < ESTUDIANTES; estudiantes++) {
-        for (int examenes = 0; examenes < EXAMENES; examenes++) {
-            if (calificaciones[estudiantes][examenes]) {
-                notaMinima = calificaciones[estudiantes][examenes];
-            }
-        }
-    }
+	int notaMinima = 0;
+	for (int estudiantes = 0; estudiantes < ESTUDIANTES; estudiantes++) {
+		for (int examenes = 0; examenes < EXAMENES; examenes++) {
+			if (calificaciones[estudiantes][examenes]) {
+				notaMinima = calificaciones[estudiantes][examenes];
+			}
+		}
+	}
 
-    return notaMinima;
+	return notaMinima;
 }
 
 int LibroCalificaciones::obtenerNotaMaxima() {
-    int notaMaxima = 0;
-    for (int estudiantes = 0; estudiantes < ESTUDIANTES; estudiantes++) {
-        for (int examenes = 0; examenes < EXAMENES; examenes++) {
-            if (calificaciones[estudiantes][examenes]) {
-                notaMaxima = calificaciones[estudiantes][examenes];
-            }
-        }
-        return notaMaxima;
-    }
+	int notaMaxima = 0;
+	for (int estudiantes = 0; estudiantes < ESTUDIANTES; estudiantes++) {
+		for (int examenes = 0; examenes < EXAMENES; examenes++) {
+			if (calificaciones[estudiantes][examenes]) {
+				notaMaxima = calificaciones[estudiantes][examenes];
+			}
+		}
+		return notaMaxima;
+	}
 }
 
 double LibroCalificaciones::ObtenerPromedio(const int[], const int) {
-    double suma = 0;
+	double suma = 0;
 
-    for (int i = 0; i <= EXAMENES; i++) {
-        suma += i;
-    }
+	for (int i = 0; i <= EXAMENES; i++) {
+		suma += i;
+	}
 
-    return suma / (EXAMENES);
+	return suma / (EXAMENES);
 }
 
 std::string LibroCalificaciones::obtenerReporteNotas() {
-    return std::string();
+	return std::string();
 }
 
 const std::string LibroCalificaciones::obtenerReporteNotasMaxMin() {
-    return std::string();
+	return std::string();
 }
 
 std::string LibroCalificaciones::getNombreCurso() {
-    return std::string();
+	return std::string();
 }
 
 void LibroCalificaciones::setNombreCurso(const std::string& nombreCurso) {
--- LibroCalificaciones.h
+++ LibroCalificaciones.h
@@ -4,22 +4,22 @@
 #include <string>
 
 class LibroCalificaciones {
-    static const int estudiantes = 10;
-    static const int examenes = 3;
-    std::string nombreCurso;
-    int calificaciones[ESTUDIANTES][EXAMENES];
-    int obtenerNotaMinima();
-    int obtenerNotaMaxima();
-    double ObtenerPromedio(const int[], const int);
+	static const int ESTUDIANTES = 10;
+	static const int EXAMENES = 3;
+	std::string nombreCurso;
+	int calificaciones[ESTUDIANTES][EXAMENES];
+	int obtenerNotaMinima();
+	int obtenerNotaMaxima();
+	double ObtenerPromedio(const int[], const int);
 
 public:
-    LibroCalificaciones();
-    LibroCalificaciones(const std::string& nombreCurso, int[][EXAMENES]);
-    std::string obtenerReporteNotas();
+	LibroCalificaciones();
+	LibroCalificaciones(const std::string& nombreCurso, int[][EXAMENES]);
+	std::string obtenerReporteNotas();
 
-    const std::string obtenerReporteNotasMaxMin();
-    std::string getNombreCurso();
-    void setNombreCurso(const std::string& nombreCurso);
+	const std::string obtenerReporteNotasMaxMin();
+	std::string getNombreCurso();
+	void setNombreCurso(const std::string& nombreCurso);
 
 };
 
